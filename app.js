9f17d303dfe36a447a1f8e18b22fdd5d6129ee77
HectorOrostica95 <hectororostica2.0@gmail.com>
2018-09-26 02:46:55 -0300

    Termino de la primera   seccion del curso de JavaScript

---
+++ app.js
@@ -0,0 +1,40 @@
+//var a = 1;
+//var b = 'Hector';
+
+//console.log(a);
+//console.warn(a);
+//console.error(a);
+//console.info(a);
+
+//console.log(a + a);
+
+//  --------
+/*
+console.log(a);
+console.log(b);
+console.log(c);
+
+var a = 'Hector';
+var b = 3;
+var c = true;
+
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+
+//  --------
+
+//function imprimir (){
+//    for (let x = 0; x < 8000; x++) {
+//        console.log('imprimir');
+//    }
+//}
+
+//function presionoClick(){
+//    console.log("Click en boton");
+//}
+
+//imprimir();
+
+var nombre = 'Hector';
