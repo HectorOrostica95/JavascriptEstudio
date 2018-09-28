cb280af12c15bced861b8f176d8c679b1c57e07b
HectorOrostica95 <hectororostica2.0@gmail.com>
2018-09-28 16:30:51 -0300

    Inicio de la 5° seccion

---
+++ 5° Seccion/app.js
@@ -0,0 +1,20 @@
+
+
+var i = 0;
+/*
+while ( i<= 10) {
+    i++;
+
+    //if (i === 5){ break; }
+    if (i === 5){ continue; }
+    console.log( i );
+}
+*/
+do{
+    i++;
+    
+    if (i === 5){ continue; }
+    
+    console.log( i );
+    
+}while (i < 10);
